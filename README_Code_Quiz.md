ac4fc5255e631d5cd68c22abd34a1f865d2a139b
chris-noriega14 <chris.noriega14@hotmail.com>
2021-10-10 23:59:41 -0500

    User can now answer all 5 questions, input initials and receive a high score, go back to the title page, and clear highscores. Timer also subtracts 15 seconds for every wrong answer.

--- README_Code_Quiz.md
+++ README_Code_Quiz.md
@@ -1,15 +1,15 @@
 Homework #4: Code Quiz
 
-Here is the screenshot of the deployed application:
-
-Here is the GitHub pages URL of the deployed application: https://chris-noriega14.github.io/Code_Quiz/
+GitHub pages URL of the deployed application: https://chris-noriega14.github.io/Code_Quiz/
 
 Webpage Description:
 
 The initial page contains the title of the quiz (Developer Quiz) and a Start Quiz button. The 5 question quiz occurs when you click the Start Quiz button. A timer of 75 seconds gets initialized as well.
 
-The updated page displays the first question of the quiz with 4 different answer choices. The user chooses one of the 4 choices. Depending on the answer choice, a text of Correct! or Wrong! will display on the page and the next question will display. This will be repeated 4 more times if the user responds to all questions within the 75 second time limit, then your final score will be displayed on the webpage. If the timer ends, your final score will be displayed on the webpage. The user will obtain 5 points for every correct answer, and 0 points for every incorrect answer.
+The updated page displays the first question of the quiz with 4 different answer choices. The user chooses one of the 4 choices. Depending on the answer choice, a text of Correct! or Wrong! will display on the page and the next question will display. Any incorrect answer subtracts 15 seconds off of the timer. This will be repeated 4 more times if the user responds to all questions within the 75 second time limit, then your final score will be displayed on the webpage. If the timer ends, your final score will be displayed on the webpage.
 
 You will be able to add your initials and final score to the current list of highscores by typing in your initials and pressing Submit.
 
 Once you press Submit, your score should be displayed in a highscore section of the webpage. There is also a button that will clear all of the saved highscores.
+
+Here is the screenshot of the deployed application: 
--- script.js
+++ script.js
@@ -1,103 +1,192 @@
-//This section displays the list of variables that will be used in the webpage.
 var instructions = document.querySelector("#container")
 var QuizTitle = document.querySelector("#title");
 var QuizStart = document.querySelector("#beginQuiz");
-var timerElement = document.querySelector("#timerStart");
-var highscore = [];
+var timerEl = document.querySelector("#timerStart");
+var choicesEl = document.querySelector("#choices");
+var Scores = [];
 var chosenQuestion = "";
+var questionIndex = 0;
 var timerCount;
-var userScore = 0;
 var QuizDone = false;
-
-//This section of code creates an array of questions, answers, and correct answers for the Quiz.
-const Questions = [
+var questions = [
     {
-        question: "Which element is not considered a commonly used data type?",
-        answers: {
-            a: "Boolean",
-            b: "String",
-            c: "Prompt",
-            d: "Integer"
-        },
-        correctAnswer: "c"
+        title: "Which element is not considered a commonly used data type?",
+        choices: [
+            "Boolean",
+            "String",
+            "Prompt",
+            "Integer"
+        ],
+        answer: "Prompt"
     },
     {
-        question: "Which tool is considered the skeleton of the webpage?",
-        answers: {
-            a: "HTML",
-            b: "CSS",
-            c: "Javascript",
-            d: "Bootstrap"
-        },
-        correctAnswer: "a"
+        title: "Which tool is considered the skeleton of the webpage?",
+        choices: [
+            "HTML",
+            "CSS",
+            "Javascript",
+            "Bootstrap"
+        ],
+        answer: "HTML"
     },
     {
-        question: "An application used data from a software intermediary in order to check weather conditions. This is an example of using what?",
-        answers: {
-            a: "API",
-            b: "HTML",
-            c: "DOM",
-            d: "CSS"
-        },
-        correctAnswer: "a"
+        title: "An application used data from a software intermediary in order to check weather conditions. This is an example of using what?",
+        choices: [
+            "API",
+            "HTML",
+            "DOM",
+            "CSS"
+        ],
+        answer: "API"
     },
     {
-        question: "Which assignment operator signifies equality in value and data type?",
-        answers: {
-            a: "!=",
-            b: "===",
-            c: "==",
-            d: ">="
-        },
-        correctAnswer: "b"
+        title: "Which assignment operator signifies equality in value and data type?",
+        choices: [
+            "!=",
+            "===",
+            "==",
+            ">="
+        ],
+        answer: "==="
     },
     {
-        question: "Which option is not considered a type of popup box in Javascript?",
-        answers: {
-            a: "Alert box",
-            b: "Confirm box",
-            c: "Prompt box",
-            d: "Denial box"
-        },
-        correctAnswer: "d"
+        title: "Which option is not considered a type of popup box in Javascript?",
+        choices: [
+            "Alert box",
+            "Confirm box",
+            "Prompt box",
+            "Denial box"
+        ],
+        answer: "Denial box"
     }
 ];
 
-//This section of code starts the Quiz and Timer 
 QuizStart.addEventListener('click',function startQuiz() {
     timerCount = 75;
-    QuizStart.disabled = true;
+    QuizStart.setAttribute("class","hide");
     getQuestion()
     startTimer()
-    timerElement.textContent = "Timer: " + timerCount;
+    timerEl.textContent = "Timer: " + timerCount;
 });
 
-//This is a function for the Quiz Timer. The timer counts down one second at a time and the timer starts based on the value on the timerCount variable in the startQuiz function above.
 function startTimer () {
     timer = setInterval(function() {
         timerCount--;
-        timerElement.textContent = "Timer: " + timerCount;
-        if (timerCount >= 0) {
+        timerEl.textContent = "Timer: " + timerCount;
+        if (timerCount > 0) {
             if(QuizDone && timerCount > 0) {
                 clearInterval(timer);
                 displayScore();
             }
         }
-            if (timerCount === 0) {
-                clearInterval(timer);
-                displayScore();
-            }
-    },1000);
-       
+        if (timerCount <= 0) {
+            quizEnd();
+            displayScore();
+        }  
+    },1000);   
 };
 
-//This function generates the 5 questions of the quiz
 function getQuestion() {
-    QuizTitle.textContent = Questions[0].question;
-    QuizStart.textContent = null;
+    var currentQuestion = questions[questionIndex];
+    var questionTitle = currentQuestion.title;
+    QuizTitle.textContent = questionTitle;
+    console.log();
+    choicesEl.innerHTML = "";
+    currentQuestion.choices.forEach(function(choice, i) {
+        var questionNode = document.createElement("button");
+        questionNode.setAttribute("class", "choice");
+        questionNode.setAttribute("value", choice);
+        questionNode.textContent = choice;
+        questionNode.onclick = checkAnswer;
+        choicesEl.appendChild(questionNode);
+    });
 };
 
-//This function displays your final score
+function checkAnswer() {
+    if (this.value !== questions[questionIndex].answer) {
+        // penalize time
+        timerCount -= 15;
+        if (timerCount < 0) {
+         timerCount = 0;
+         console.log("Wrong!");
+        }
+        timerEl.textContent = timerCount;
+       } else {
+           console.log("Correct!");
+        }
+       // move to next question
+      questionIndex++;
+       // check if we've run out of questions
+       if (questionIndex === questions.length) {
+        quizEnd();
+       } else {
+        getQuestion();
+       }
+      }
+
+function quizEnd() {
+    clearInterval(timer);
+    displayScore();
+}
+
 function displayScore() {
-   instructions.textContent = "Your final score is: " + userScore;
+    var buttonSubmitArea = document.createElement("div");
+    var buttonSubmit = document.createElement("button");
+    var submitScore = document.createTextNode("Submit");
+    var inputInitials = document.createElement("textarea");
+    var requestInitials = document.createElement("p");
+    var userInitials = document.createElement("p");
+    var currentScore;
+    
+    
+    instructions.setAttribute("style","white-space: pre;");
+    instructions.textContent = "Quiz Complete!\r\n";
+    instructions.textContent +="Your final score is:" + timerCount + "\r\n";
+    requestInitials.textContent +="Enter Initials:";
+    
+    requestInitials.appendChild(userInitials);
+    instructions.appendChild(requestInitials);
+    instructions.appendChild(inputInitials);
+    instructions.appendChild(buttonSubmitArea);
+    buttonSubmit.appendChild(submitScore);
+    buttonSubmitArea.appendChild(buttonSubmit);
+    
+
+    buttonSubmit.addEventListener("click",function() {
+        currentScore = inputInitials.value + " - " + timerCount;
+        Scores.push(currentScore);
+        localStorage.setItem('Scores',JSON.stringify(currentScore));
+        getHighscores();
+    })
 };
+
+function getHighscores() {
+    var highscoreListArea = document.createElement("div");
+    var highscoreList = document.createTextNode(localStorage.getItem("Scores"));
+    var goBackButtonArea = document.createElement("div");
+    var goBackButton = document.createElement("button");
+    var goBackButtonText = document.createTextNode("Go Back");
+    var clearHighscoresArea = document.createElement("div");
+    var clearHighscoresButton = document.createElement("button");
+    var clearHighscoresText = document.createTextNode("Clear Highscores");
+    instructions.textContent = "Highscores";
+
+    goBackButton.addEventListener("click", function() {
+        window.location.href="index.html";
+    })
+
+    clearHighscoresButton.addEventListener("click", function() {
+        localStorage.clear("Scores");
+        highscoreList.textContent = "";
+    })
+
+    instructions.appendChild(highscoreListArea);
+    instructions.appendChild(goBackButtonArea);
+    instructions.appendChild(clearHighscoresArea);
+    highscoreListArea.appendChild(highscoreList);
+    goBackButtonArea.appendChild(goBackButton);
+    goBackButton.appendChild(goBackButtonText);
+    clearHighscoresArea.appendChild(clearHighscoresButton);
+    clearHighscoresButton.appendChild(clearHighscoresText);
+
+}
